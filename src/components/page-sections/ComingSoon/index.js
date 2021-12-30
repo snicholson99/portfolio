4f3671b1bdc1d70ad2b7b6558e0633c3896a3245
Scott Nicholson <scott.nicholson@live.co.uk>
2021-12-30 22:00:14 +0000

    feat: adding particles to coming soon page

--- src/components/page-sections/ComingSoon/index.js
+++ src/components/page-sections/ComingSoon/index.js
@@ -1,9 +1,17 @@
+import Particles from "react-tsparticles";
 import './style.css';
+import { particlesOptions } from "./particlesOptions";
 
 const ComingSoon = () => (
   <div id="coming-soon" className="page-section">
-    <p>Further content coming soon...</p>
-    <p>Why not check out <a href="https://github.com/snicholson99" target="_blank" rel="noreferrer">my GitHub profile</a>?</p>
+    <Particles
+      id="tsparticles"
+      options={particlesOptions}
+    />
+    <div id="coming-soon-text-container">
+      <p>Further content coming soon...</p>
+      <p>Why not check out <a href="https://github.com/snicholson99" target="_blank" rel="noreferrer">my GitHub profile</a>?</p>
+    </div>
   </div>
 );
 
